a6bfdbfabcb53a33e4369d84cec1a73362766c62
Preeti Mallick <preetimallick@Preetis-MacBook-Pro.local>
2022-05-21 15:03:05 +0530

    amazon-clone header created

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB8JEdSGCf6huGgjDOQpsMMk32yg8fltTU",
+    authDomain: "challenge-b657d.firebaseapp.com",
+    projectId: "challenge-b657d",
+    storageBucket: "challenge-b657d.appspot.com",
+    messagingSenderId: "719138785333",
+    appId: "1:719138785333:web:089a2ebc1b32812f5bc97c",
+    measurementId: "G-S4TL74VHE4"
+  };
